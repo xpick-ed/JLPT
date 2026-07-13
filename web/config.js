9253b13179b0d953e12ea93d62617a564c4dd540
eslin <edward_lin@gmail.com>
2026-07-14 00:19:43 +0800

    chore: set worker url + client id

--- web/config.js
+++ web/config.js
@@ -1,2 +1,2 @@
-export const WORKER_URL = "";        // deployed Worker URL, e.g. https://vocab-sync.you.workers.dev
-export const GOOGLE_CLIENT_ID = "";  // Google OAuth Client ID (public value)
+export const WORKER_URL = "https://vocab-sync.leh-x.workers.dev";        // deployed Worker URL, e.g. https://vocab-sync.you.workers.dev
+export const GOOGLE_CLIENT_ID = "320699564075-6d0bm66o5bto68vsphog12tqm3m86emk.apps.googleusercontent.com";  // Google OAuth Client ID (public value)
